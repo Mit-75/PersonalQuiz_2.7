2a20e61588dee4d88262307b53af662343e27383
Dmitry Parkhomenko <oku_74@mail.ru>
2023-11-30 09:26:41 +0500

    Initial Commit

---
+++ PersonalQuiz_2.7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz_2.7
+//
+//  Created by Dmitry Parhomenko on 30.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
